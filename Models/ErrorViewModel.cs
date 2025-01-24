fc380fa454a7afe1dfe1f5a4b5d7be792a9fb0d1
trapperwetzel <trapperwetzel@gmail.com>
2025-01-24 00:11:06 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CIS_414_Playlist_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
